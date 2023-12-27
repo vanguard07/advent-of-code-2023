6b2ec9fed9093cd998f7adf86b96ac1217abfc05
Avtans Kumar <avtans@gmail.com>
2023-12-27 14:37:04 -0600

    :sparkles: Add Day 9: Mirage Maintenance solution

---
+++ day09/q2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main ()  {
+	file, err := os.Open("day09/input.txt")
+	check(err)
+
+	scanner := bufio.NewScanner(file)
+	ans := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		numStrings := strings.Fields(line)
+		var nums []int
+		for _, numStr := range numStrings {
+			num, _ := strconv.Atoi(numStr)
+			nums = append(nums, num)
+		}
+		var firstNums []int
+		firstNums = append(firstNums, nums[0])
+
+		zeroCount := 0
+		for (len(nums) != zeroCount) {
+			var temp []int
+			zeroCount = 0
+			for i := 0; i < len(nums) - 1; i++ {
+				diff := nums[i + 1] - nums[i]
+				if (diff == 0) {
+					zeroCount += 1
+				}
+				temp = append(temp, diff)
+			}
+			nums = temp
+			firstNums = append(firstNums, nums[0])
+		}
+		res := 0
+		for i := len(firstNums) - 1; i >= 0; i-- {
+			res = firstNums[i] - res
+		}
+		ans += res
+	}
+	file.Close()
+	fmt.Println(ans)
+}
